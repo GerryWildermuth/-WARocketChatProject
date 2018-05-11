5c3fc9bc225d2b85999b24ea7ba99d518ed1daff
Gerrit Wildermuth <gerrit.wildermuth@systecs.com>
2018-05-11 11:24:28 +0200

    small Changes

--- SWARocketChat/SWARocketChat/Models/Entites.cs
+++ SWARocketChat/SWARocketChat/Models/Entites.cs
@@ -1,89 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace SWARocketChat.Models
 {
-    public class App
+    public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         [Required]
-        public string AppName { get; set; }
-        public virtual ICollection<AppLanguage> AppLanguages { get; set; }
-        public virtual ICollection<AppLocation> AppLocations { get; set; }
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public string UserImage { get; set; }
+
     }
 
-    public class Region
+    public class RocketChatContext : DbContext
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        [Required]
-        public string RegionName { get; set; }
-        public string Manager { get; set; }
-        public virtual ICollection<Location> Locations { get; set; }
-    }
 
-    public class Location
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        [Required]
-        [StringLength(5)]
-        public string GeoCode { get; set; }
-        public virtual Region Regions { get; set; }
-        [Required]
-        public Guid RegionId { get; set; }
-        public virtual ICollection<AppLocation> AppLocations { get; set; }
-    }
-
-    public class AppLocation
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        public Guid LocationId { get; set; }
-        public virtual Location Location { get; set; }
-        [Required]
-        public Guid AppId { get; set; }
-        public virtual App App { get; set; }
-
-        [Required, Url]
-        public string Url { get; set; }
-    }
-
-    public class AppLanguage
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        [Required]
-        public Guid AppId { get; set; }
-        public virtual App App { get; set; }
-        [Required, MinLength(5, ErrorMessage = "Image must not be empty")]
-        public string Image { get; set; }
-        public string Thumbnail { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public string Description { get; set; }
-        [Required]
-        public Guid LanguageId { get; set; }
-        public Language Language { get; set; }
-    }
-
-    public class Language
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        [Required, MaxLength(3), MinLength(2)]
-        public string LanguageCode { get; set; }
-        public string LanguageName { get; set; }
-        public string Image { get; set; }
-    }
-
-    public class ToolboxContext : DbContext
-    {
-        public DbSet<App> Apps { get; set; }
-        public DbSet<Region> Regions { get; set; }
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<AppLocation> AppLocations { get; set; }
-        public DbSet<AppLanguage> AppLanguages { get; set; }
-        public DbSet<Language> Languages { get; set; }
+        public DbSet<User> Users { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
