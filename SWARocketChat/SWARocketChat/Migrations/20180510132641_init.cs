86e771f8183901ac47db3daa71fc725d3c6258c3
Gerrit Wildermuth <gerrit.wildermuth@systecs.com>
2018-05-11 10:47:43 +0200

    Initial Commit

---
+++ SWARocketChat/SWARocketChat/Migrations/20180510132641_init.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SWARocketChat.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apps",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AppName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    LanguageCode = table.Column<string>(maxLength: 3, nullable: false),
+                    LanguageName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Manager = table.Column<string>(nullable: true),
+                    RegionName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppLanguages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AppId = table.Column<Guid>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: false),
+                    LanguageId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Thumbnail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppLanguages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppLanguages_Apps_AppId",
+                        column: x => x.AppId,
+                        principalTable: "Apps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppLanguages_Languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    GeoCode = table.Column<string>(maxLength: 5, nullable: false),
+                    RegionId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Locations_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppLocations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AppId = table.Column<Guid>(nullable: false),
+                    LocationId = table.Column<Guid>(nullable: false),
+                    Url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppLocations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppLocations_Apps_AppId",
+                        column: x => x.AppId,
+                        principalTable: "Apps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppLocations_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLanguages_AppId",
+                table: "AppLanguages",
+                column: "AppId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLanguages_LanguageId",
+                table: "AppLanguages",
+                column: "LanguageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLocations_AppId",
+                table: "AppLocations",
+                column: "AppId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLocations_LocationId",
+                table: "AppLocations",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_RegionId",
+                table: "Locations",
+                column: "RegionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppLanguages");
+
+            migrationBuilder.DropTable(
+                name: "AppLocations");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "Apps");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
---
+++ SWARocketChat/SWARocketChat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SWARocketChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
