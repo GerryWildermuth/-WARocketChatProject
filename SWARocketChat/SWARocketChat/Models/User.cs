0a3aa295166a302770fd747265716a9a662d69b6
Gerrit Wildermuth <g.wildermuth@outlook.de>
2018-05-26 13:51:02 +0200

    one change

--- SWARocketChat/SWARocketChat/Models/User.cs
+++ SWARocketChat/SWARocketChat/Models/User.cs
@@ -13,6 +13,11 @@ namespace SWARocketChat.Models
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password2 { get; set; }
 
         public string UserImage { get; set; }
         [Required]
