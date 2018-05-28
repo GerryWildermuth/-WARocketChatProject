59f61dd9d4794bb41352c81cf83cdf5a9cd6a41f
Gerrit Wildermuth <g.wildermuth@outlook.de>
2018-05-28 19:01:57 +0200

    Revert "no Content"
    This reverts commit 635b02f4beef61b967c14bc022443dddbb0d8ac2.

---
+++ SWARocketChat/SWARocketChat/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWARocketChat.Models;
+
+namespace SWARocketChat.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected BaseController() { }
+
+        protected readonly RocketChatContext DbContext = new RocketChatContext();
+    }
+}
---
+++ SWARocketChat/SWARocketChat/Controllers/LoginController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SWARocketChat.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWARocketChat.Controllers
+{
+    [Route("Login")]
+    public class LoginController : BaseController
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(new User());
+        }
+
+        [HttpPost("")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index([Bind("Id,Username,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var thisuser = DbContext.Users.FirstOrDefault(r => r.Username == user.Username);
+                if (thisuser != null && thisuser.Username == user.Username && thisuser.Password == user.Password)
+                    return RedirectToAction("Index", "Home");
+                ModelState.AddModelError("", "Username or Password not correct");
+            }
+            return View(user);
+        }
+        [HttpGet("ForgotPassword")]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        
+        [HttpGet("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost("Register")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Id,Username,Password,Password2,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DbContext.Users.Any(r => r.Username == user.Username))
+                {
+                    ModelState.AddModelError("", "Username already exist");
+                    //return RedirectToAction(nameof(Register));
+                }
+                if (DbContext.Users.Any(r => r.Email == user.Email))
+                {
+                    ModelState.AddModelError("", "Email already exist");
+                }
+
+                if (user.Password!=user.Password2)
+                {
+                    ModelState.AddModelError("", "Password dont match");
+                }
+                else
+                {
+                    DbContext.Add(user);
+                    await DbContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                
+            }
+            return View(user);
+        }
+        private bool UserExists(Guid id)
+        {
+            return DbContext.Users.Any(e => e.Id == id);
+        }
+    }
+}
