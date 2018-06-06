728d9d1f00ed113a0ad0b1f852dd93f7d7d65e02
Gerrit Wildermuth <g.wildermuth@outlook.de>
2018-06-06 20:41:32 +0200

    Channel New Right Side Design

--- SWARocketChat/SWARocketChat/wwwroot/js/site.js
+++ SWARocketChat/SWARocketChat/wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Write your JavaScript code.
-$(function () {
+$(function() {
     $('[data-toggle="tooltip"]').tooltip();
-})
+});
+
+var SmallTable = $("#DatatableSmall").DataTable(
+    {
+        //scrollY: "74vh",
+        //scrollCollapse: true,
+        hover: true
+    });
+//$("#DatatableVerticalScroll_filter").hide();
+//$(function () {
+//    $("#SearchInput").on("keyup", function () {
+//        var filter = $(this).val();
+//        regex = "\\b" + filter + "\\b";
+//        SmallTable.search(filter, true, false).draw();
+//    });
+//});
