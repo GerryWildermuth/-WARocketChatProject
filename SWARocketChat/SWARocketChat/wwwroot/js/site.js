889fedcc98df2764320f4b38d0ff5d5cb9094b5c
GerryWildermuth <gerry1313@web.de>
2018-06-08 09:32:32 +0200

    DataTables-InChannels+HomeBootstrapIncludeFix

--- SWARocketChat/SWARocketChat/wwwroot/js/site.js
+++ SWARocketChat/SWARocketChat/wwwroot/js/site.js
@@ -7,13 +7,22 @@ var SmallTable = $("#DatatableSmall").DataTable(
     {
         //scrollY: "74vh",
         //scrollCollapse: true,
-        hover: true
+        hover: true,
+        sort:false
     });
-//$("#DatatableVerticalScroll_filter").hide();
-//$(function () {
-//    $("#SearchInput").on("keyup", function () {
-//        var filter = $(this).val();
-//        regex = "\\b" + filter + "\\b";
-//        SmallTable.search(filter, true, false).draw();
-//    });
-//});
+var ChannelTable = $("#DatatableChannel").DataTable(
+    {
+        scrollY: "100vh",
+        scrollCollapse: true,
+        "paging": false,
+        "ordering": false,
+        "info": false
+    });
+$("#DatatableChannel_filter").hide();
+$(function () {
+    $("#SearchInput").on("keyup", function () {
+        var filter = $(this).val();
+        regex = "\\b" + filter + "\\b";
+        SmallTable.search(filter, true, false).draw();
+    });
+});
