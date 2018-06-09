151adcd36ed1679abca770ff49289cc9f8536569
Gerrit Wildermuth <g.wildermuth@outlook.de>
2018-06-09 10:32:41 +0200

    ASP NET CORE 2.1 Upgrade SignalR compatible

--- SWARocketChat/SWARocketChat/Startup.cs
+++ SWARocketChat/SWARocketChat/Startup.cs
@@ -37,6 +37,8 @@ namespace SWARocketChat
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +64,10 @@ namespace SWARocketChat
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chathub");
             });
         }
     }
