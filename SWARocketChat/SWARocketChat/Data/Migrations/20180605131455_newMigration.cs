b346a87063f1ce37c6fd07d18aa0290b05fc8fb6
Fauth, Patrick <patrick.fauth@gmx.de>
2018-06-13 09:11:18 +0200

    minor changes

---
+++ SWARocketChat/SWARocketChat/Data/Migrations/20180605131455_newMigration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SWARocketChat.Data.Migrations
+{
+    public partial class newMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_UserId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Messages",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_UserId",
+                table: "Messages",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_UserId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Messages",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_UserId",
+                table: "Messages",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
